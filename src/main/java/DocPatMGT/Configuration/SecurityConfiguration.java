2778565b2ec90ee0c090683f417f2c1e7354b7e4
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-10-03 12:33:44 +0530

    User Registration Completed / Security Configuration Initialized

--- src/main/java/DocPatMGT/Configuration/SecurityConfiguration.java
+++ src/main/java/DocPatMGT/Configuration/SecurityConfiguration.java
@@ -39,9 +39,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/patient/register").permitAll() // Allow access to this endpoint without authentication
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/patient/register").permitAll()
+//                .antMatchers("/patient/register").hasAuthority("PATIENT")
+                .antMatchers("/doctor/register").permitAll()
+//                .antMatchers("/doctor/register").hasAuthority("DOCTOR")
                 .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
